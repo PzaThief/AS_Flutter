1fe6bf9b8cf06a86822608182c409ba37e88d542
PzaThief <vwjdalsgkv@gmail.com>
2021-01-25 05:56:53 +0900

    input image in homescreen

--- flutter_app/lib/screen/screen_home.dart
+++ flutter_app/lib/screen/screen_home.dart
@@ -3,10 +3,36 @@ import 'package:flutter/rendering.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _HomeSreenState createState() => _HomeSreenState();
 }
 
 class _HomeSreenState extends State<HomeScreen> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Nyan App'),
+          backgroundColor: Colors.deepPurple,
+          leading: Container(),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/cat@2x.png',
+                width: width * 0.8,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
