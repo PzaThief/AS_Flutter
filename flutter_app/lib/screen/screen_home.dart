202f275b9b6e5c24a682fc9d20c013d59e14c6fc
PzaThief <vwjdalsgkv@gmail.com>
2021-01-26 00:54:09 +0900

    delete goback in homescreen

--- flutter_app/lib/screen/screen_home.dart
+++ flutter_app/lib/screen/screen_home.dart
@@ -32,77 +32,79 @@ class _HomeSreenState extends State<HomeScreen> {
     double width = screenSize.width;
     double height = screenSize.height;
 
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('My Nyan App'),
-          backgroundColor: Colors.deepPurple,
-          leading: Container(), //앱바상 자동으로 생겨나는 버튼 제거용
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Image.asset(
-                'images/cat@2x.png',
-                width: width * 0.7,
-              ),
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('My Nyan App'),
+              backgroundColor: Colors.deepPurple,
+              leading: Container(), //앱바상 자동으로 생겨나는 버튼 제거용
             ),
-            Padding(
-              padding: EdgeInsets.all(width * 0.024),
-            ),
-            Text(
-              '플러터 퀴즈 앱',
-              style: TextStyle(
-                fontSize: width * 0.065,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '퀴즈를 풀기 전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
-              textAlign: TextAlign.center,
-            ),
-            Padding(
-              padding: EdgeInsets.all(width * 0.048),
-            ),
-            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
-            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
-            _buildStep(width, '3. 만점을 향해 도전해보세요!'),
-            Padding(
-              padding: EdgeInsets.all(width * 0.048),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: width * 0.036),
-              child: Center(
-                child: ButtonTheme(
-                  minWidth: width * 0.8,
-                  height: height * 0.05,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: RaisedButton(
-                    child: Text(
-                      '지금 퀴즈 풀기',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    color: Colors.deepPurple,
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => QuizScreen(
-                                    quizs: quizs,
-                                  )));
-                    },
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'images/cat@2x.png',
+                    width: width * 0.7,
                   ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                Padding(
+                  padding: EdgeInsets.all(width * 0.024),
+                ),
+                Text(
+                  '플러터 퀴즈 앱',
+                  style: TextStyle(
+                    fontSize: width * 0.065,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '퀴즈를 풀기 전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.048),
+                ),
+                _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+                _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
+                _buildStep(width, '3. 만점을 향해 도전해보세요!'),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.048),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: width * 0.036),
+                  child: Center(
+                    child: ButtonTheme(
+                      minWidth: width * 0.8,
+                      height: height * 0.05,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: RaisedButton(
+                        child: Text(
+                          '지금 퀴즈 풀기',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.deepPurple,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => QuizScreen(
+                                        quizs: quizs,
+                                      )));
+                        },
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 
   Widget _buildStep(double width, String title) {
